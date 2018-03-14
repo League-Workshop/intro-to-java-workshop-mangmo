f8c1a9a77bd12c374684267ca41dd589202194e4
mangmo <league@WorthyMini-5.attlocal.net>
2018-03-13 19:56:44 -0700

    my name is jeff

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -4,30 +4,49 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
-	
+
 	// 2. Create a new Robot
+	Robot jeff = new Robot();
+	
 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		jeff.setSpeed(1000);
+		jeff.moveTo(100,700);
 		// 5. Set the pen width to 5
+		jeff.setPenWidth(5);
+		jeff.penDown();
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+		for (int i = 0; i < 4; i++) {
+			jeff.turn(90);
+			jeff.move(200);
+			for (int j = 0; j < 4; j++) {
+				jeff.turn(100);
+				jeff.move(130);
+			}
 			// 7. Set the pen color to random
-	
+			jeff.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+			drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+			jeff.turn(90);
+		}
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		jeff.turn(90);
+		jeff.move(100);
+		jeff.turn(90);
+		jeff.move(100);
+		jeff.turn(90);
+		jeff.move(100);
+		jeff.turn(90);
+		jeff.move(100);
+		jeff.turn(90);
+		jeff.move(100);
 
-		
-		
 	}
 
 	public static void main(String[] args) {
@@ -35,6 +54,3 @@ public class FourSquare {
 	}
 
 }
-
-
-
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
-		// START HERE
-	
-		
-		
+
+		Robot bb8 = new Robot();
+		bb8.setPenColor(Color.BLUE);
+		bb8.penDown();
+		bb8.sparkle();
+		bb8.setSpeed(10);
+		bb8.miniaturize();
+		for (int i = 0; i < 100; i++) {
+			bb8.move(105);
+			bb8.turn(90);
+		}
 	}
 }
