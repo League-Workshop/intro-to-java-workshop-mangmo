843b1f4f1b1726139037a29990efd46dcc056af6
mangmo <league@WTS-IM-6.attlocal.net>
2018-03-14 19:58:58 -0700

    mad libs

---
+++ src/section3/Ugandinknuckles.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Ugandinknuckles {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null, "ur so amazingly terrible");	
+JOptionPane.showMessageDialog(null, "i love to hate you");
+JOptionPane.showMessageDialog(null, "i would rather die then to be married to you");
+JOptionPane.showMessageDialog(null, "why are you so not perfect");
+JOptionPane.showMessageDialog(null, "oh btw ur hair looks nice it's also opposite day");
+}
+}
+
---
+++ src/section3/snooper.java
@@ -0,0 +1,10 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class snooper {
+public static void main(String[] args) {
+String ugandinknuckles = JOptionPane.showInputDialog("are you a girl or a boy");
+JOptionPane.showMessageDialog(null, "hi"+ugandinknuckles);
+}
+}
