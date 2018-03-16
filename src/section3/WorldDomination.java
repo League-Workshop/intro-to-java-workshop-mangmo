47ff7ef64ea2868a6ef4972993ecd354d3223667
mangmo <league@WTS-IM-6.attlocal.net>
2018-03-15 19:57:39 -0700

    noadifnaosdf

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,13 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+      String blah=JOptionPane.showInputDialog("do you know how to code");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, wish them good luck washing dishes.
-
+        if(blah.equals("yes")) {
+        		JOptionPane.showInputDialog("you will dominate the world");
+        }
+        else {
+     JOptionPane.showMessageDialog(null, "in the arms of the angels");
 	}
 }
-
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,34 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int Score=0; 
 		// 2.  Ask the user a question 
-		
+ String garretthedog = JOptionPane.showInputDialog("what's 9 + 10");
 		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
+	 if(garretthedog.equalsIgnoreCase("21")) {
+     JOptionPane.showConfirmDialog(null, "you know your memes well");
+	 
+	JOptionPane.showInputDialog("you have one point")	;
+	 } else {
+	JOptionPane.showMessageDialog(null, "why did you toucha my speghett");
+	 int score=1;	
+	 }
+	 JOptionPane.showInputDialog("what year was do you know de wae made in");
+	 if(garretthedog.equalsIgnoreCase("2018")) {
+	 JOptionPane.showConfirmDialog(null, "you know your memes well");
+	 JOptionPane.showInputDialog("you have two points point")	;
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
-		
+	 JOptionPane.showInputDialog("good job you have beet the MEME QUIZ you got 2 points")	;
 	}
+}
 }
